c617baa009e3f48d2f601d6ab7bb8459eb17f65a
FFNight <teetingyi@live.com>
2017-02-15 14:49:09 +0800

    Add ChooseRoleActivity.java
    This should be the new main page (User no need to enter string for demo)

---
+++ app/src/main/java/testapp/ttyi/certisme/ChooseRoleActivity.java
@@ -0,0 +1,45 @@
+package testapp.ttyi.certisme;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class ChooseRoleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_role);
+
+        Button visitor_btn = (Button) findViewById(R.id.visitor_btn);
+        Button officer_btn = (Button) findViewById(R.id.officer_btn);
+
+        // If Visitor button is pressed
+        visitor_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                    Intent intent = new Intent(ChooseRoleActivity.this, QRTutActivity.class);
+                    intent.putExtra("EXTRA_FIN", "A1234567A"); // Hardcoded FIN just for the demo XD
+                    startActivity(intent);
+                }
+        });
+
+        // If APO button is pressed
+        officer_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                Intent intent = new Intent(ChooseRoleActivity.this, QRTutActivity.class);
+                intent.putExtra("EXTRA_FIN", "A1234567A"); // Hardcoded FIN just for the demo XD
+                startActivity(intent);
+            }
+        });
+
+    }
+}
